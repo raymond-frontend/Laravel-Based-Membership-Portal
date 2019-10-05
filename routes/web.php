d3648e7d3e971b922e4c6fc84a05e573738e1b37
olasunkanmi oyinlola <oyinolasunkanmi@gmail.com>
2019-10-05 12:00:06 +0800

    Email verification enabled

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,8 @@ Route::get('/', function () {
 
 Auth::routes(['verify' => true]);
 
-//Route::get('home', 'HomeController@index')->name('home')->middleware('verified');
-Route::get('home', 'HomeController@index')->name('home');
+Route::get('home', 'HomeController@index')->name('home')->middleware('verified');
+//Route::get('home', 'HomeController@index')->name('home');
 
 /****Route::get('/csv', 'CsvFile@index')->name('importPage');
 Route::post('csv/import', 'CsvFile@csv_import')->name('import');***/
